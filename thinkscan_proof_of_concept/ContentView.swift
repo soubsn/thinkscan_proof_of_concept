c559ea513d6c1b50e240eab82eb47efa7b49938e
soubsn <45074529+soubsn@users.noreply.github.com>
2025-11-27 15:50:42 -0500

    Initial Commit

---
+++ thinkscan_proof_of_concept/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thinkscan_proof_of_concept
+//
+//  Created by Nicolas Soubry on 2025-11-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ thinkscan_proof_of_concept/thinkscan_proof_of_conceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  thinkscan_proof_of_conceptApp.swift
+//  thinkscan_proof_of_concept
+//
+//  Created by Nicolas Soubry on 2025-11-27.
+//
+
+import SwiftUI
+
+@main
+struct thinkscan_proof_of_conceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
